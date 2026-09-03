9ca8a6dd992f55171f19f3937029979640c8226f
gabriel prado wender <141036938+gabrielwenderprado@users.noreply.github.com>
2026-09-03 07:41:00 -0300

    Update team scores for match on 25 AGO 2026

--- data.js
+++ data.js
@@ -5,5 +5,5 @@ const weeks={
   2:{date:'18 AGO 2026',teams:[['TIME 1',['Dudu','Rosivaldo','Du'],14,true],['TIME 2',['Gabriel','Lucas','Felipe'],18,false]]},
   3:{date:'25 AGO 2026',teams:[['TIME 1',['Lucas','Felipe','Rosivaldo'],5,false],['TIME 2',['Gabriel','Du','dudu'],4,true]]},
   4:{date:'02 SET 2026',teams:[['TIME 1',['Lucas','Felipe','Rosivaldo'],7,true],['TIME 2',['Dudu','Du','Gabriel'],3,false]]},
-  5:{date:'25 AGO 2026',teams:[['TIME 1',['Gabriel','lucas','du'],2,false],['TIME 2',['rosivaldo','dudu','felipe'],0,true]]}
+  5:{date:'25 AGO 2026',teams:[['TIME 1',['Gabriel','lucas','du'],3,false],['TIME 2',['rosivaldo','dudu','felipe'],3,true]]}
 };
