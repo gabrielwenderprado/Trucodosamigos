e3c47dbc3d99c827a00eda95413f2ac110020d80
gabriel prado wender <141036938+gabrielwenderprado@users.noreply.github.com>
2026-09-18 15:37:28 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+// EDITAR AQUI: altere nomes, pontos e semanas antes de publicar no GitHub.
+const players=[['Gabriel',148,'GA'],['Dudu',132,'DU'],['Du',120,'DO'],['Lucas',98,'LU'],['Felipe',84,'FE'],['Rosivaldo',72,'RO'],['Rafaela',0,'RA']];
+const weeks={
+  1:{date:'18 AGO 2026',teams:[['TIME 1',['Dudu','Gabriel','Du'],148,true],['TIME 2',['Rosivaldo','Lucas','Felipe'],10,false]]},
+  2:{date:'25 AGO 2026',teams:[['TIME 1',['Lucas','Dudu','Rosivaldo'],96,false],['TIME 2',['Gabriel','Du','Felipe'],121,true]]}
+};
