fb195462c8c387368eb155636e293596d4caa020
gabriel prado wender <141036938+gabrielwenderprado@users.noreply.github.com>
2026-09-18 15:54:47 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,6 +1,13 @@
 // EDITAR AQUI: altere nomes, pontos e semanas antes de publicar no GitHub.
-const players=[['Gabriel',148,'GA'],['Dudu',132,'DU'],['Du',120,'DO'],['Lucas',98,'LU'],['Felipe',84,'FE'],['Rosivaldo',72,'RO'],['Rafaela',0,'RA']];
+const players=[['Lucas',5,'LU'],['Gabriel',4,'GA'],['Felipe',4,'FE'],['Rosivaldo',4,'RO'],['Dudu',2,'DU'],['Du',2,'DO']];
+const drawPlayers=[...players,['Rafaela',0,'RA']];
 const weeks={
-  1:{date:'18 AGO 2026',teams:[['TIME 1',['Dudu','Gabriel','Du'],148,true],['TIME 2',['Rosivaldo','Lucas','Felipe'],10,false]]},
-  2:{date:'25 AGO 2026',teams:[['TIME 1',['Lucas','Dudu','Rosivaldo'],96,false],['TIME 2',['Gabriel','Du','Felipe'],121,true]]}
+  1:{date:'18 AGO 2026',teams:[['TIME 1',['Dudu','Gabriel','Du'],12,true],['TIME 2',['Rosivaldo','Lucas','Felipe'],10,false]]},
+  2:{date:'25 AGO 2026',teams:[['TIME 1',['Lucas','Dudu','Rosivaldo'],14,false],['TIME 2',['Gabriel','Du','Felipe'],18,true]]},
+  3:{date:'',teams:[['TIME 1',[],5,true],['TIME 2',[],4,false]]},
+  4:{date:'',teams:[['TIME 1',[],7,true],['TIME 2',[],3,false]]},
+  5:{date:'',teams:[['TIME 1',[],5,true],['TIME 2',[],3,false]]},
+  6:{date:'',teams:[['TIME 1',[],6,true],['TIME 2',[],4,false]]},
+  7:{date:'',teams:[['TIME 1',[],3,false],['TIME 2',[],5,true]]},
+  8:{date:'',teams:[['TIME 1',[],0,true],['TIME 2',[],0,false]]}
 };
