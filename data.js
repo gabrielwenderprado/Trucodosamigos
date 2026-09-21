eebeb9eb555b7d6f0b56fd26727af1e8cb016e9c
gabriel prado wender <141036938+gabrielwenderprado@users.noreply.github.com>
2026-09-21 15:56:32 -0300

    Clean up weeks 4 to 8 data in data.js
    Removed empty teams and dates for weeks 4 to 8.

--- data.js
+++ data.js
@@ -5,9 +5,4 @@ const weeks={
   1:{date:'18 AGO 2026',teams:[['TIME 1',['Dudu','Gabriel','Du'],12,true],['TIME 2',['Rosivaldo','Lucas','Felipe'],10,false]]},
   2:{date:'25 AGO 2026',teams:[['TIME 1',['Lucas','Dudu','Rosivaldo'],14,false],['TIME 2',['Gabriel','Du','Felipe'],18,true]]},
   3:{date:'21 set 2026',teams:[['TIME 1',['Lucas','Dudu','felipe'],1,false],['TIME 2',['Gabriel','Du','rosivaldo'],1,true]]},
-  4:{date:'',teams:[['TIME 1',[],7,true],['TIME 2',[],3,false]]},
-  5:{date:'',teams:[['TIME 1',[],5,true],['TIME 2',[],3,false]]},
-  6:{date:'',teams:[['TIME 1',[],6,true],['TIME 2',[],4,false]]},
-  7:{date:'',teams:[['TIME 1',[],3,false],['TIME 2',[],5,true]]},
-  8:{date:'',teams:[['TIME 1',[],0,true],['TIME 2',[],0,false]]}
 };
