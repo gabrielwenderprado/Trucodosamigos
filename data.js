3bc3333ec0f8c4b40ca910616a06526b909104f8
gabriel prado wender <141036938+gabrielwenderprado@users.noreply.github.com>
2026-09-25 13:37:16 -0300

    Update player scores and team points

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 // EDITAR AQUI: altere nomes, pontos e semanas antes de publicar no GitHub.
-const players=[['Lucas',5,'LU'],['Gabriel',4,'GA'],['Felipe',4,'FE'],['Rosivaldo',4,'RO'],['Dudu',2,'DU'],['rafaela',0,'RA'],['Du',2,'DO']];
+const players=[['Lucas',6,'LU'],['Gabriel',4,'GA'],['Felipe',5,'FE'],['Rosivaldo',4,'RO'],['Dudu',3,'DU'],['rafaela',1,'RA'],['Du',2,'DO']];
 const drawPlayers=[...players,['Rafaela',0,'RA']];
 const weeks={
   1:{date:'18 AGO 2026',teams:[['TIME 1',['Dudu','Gabriel','Du'],12,true],['TIME 2',['Rosivaldo','Lucas','Felipe'],10,false]]},
   2:{date:'25 AGO 2026',teams:[['TIME 1',['Lucas','Dudu','Rosivaldo'],14,false],['TIME 2',['Gabriel','Du','Felipe'],18,true]]},
-  3:{date:'21 set 2026',teams:[['TIME 1',['Lucas','Dudu','felipe','Rafaela'],4,false],['TIME 2',['Gabriel','Du','rosivaldo'],3,true]]},
+  3:{date:'21 set 2026',teams:[['TIME 1',['Lucas','Dudu','felipe','Rafaela'],5,false],['TIME 2',['Gabriel','Du','rosivaldo'],4,true]]},
 };
