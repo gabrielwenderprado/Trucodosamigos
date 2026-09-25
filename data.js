2241c17086e93df5146e8bbfde956add9bf6ffd7
gabriel prado wender <141036938+gabrielwenderprado@users.noreply.github.com>
2026-09-25 08:49:46 -0300

    Add Rafaela to team lineup for week 3

--- data.js
+++ data.js
@@ -4,5 +4,5 @@ const drawPlayers=[...players,['Rafaela',0,'RA']];
 const weeks={
   1:{date:'18 AGO 2026',teams:[['TIME 1',['Dudu','Gabriel','Du'],12,true],['TIME 2',['Rosivaldo','Lucas','Felipe'],10,false]]},
   2:{date:'25 AGO 2026',teams:[['TIME 1',['Lucas','Dudu','Rosivaldo'],14,false],['TIME 2',['Gabriel','Du','Felipe'],18,true]]},
-  3:{date:'21 set 2026',teams:[['TIME 1',['Lucas','Dudu','felipe'],2,false],['TIME 2',['Gabriel','Du','rosivaldo'],3,true]]},
+  3:{date:'21 set 2026',teams:[['TIME 1',['Lucas','Dudu','felipe','Rafaela'],4,false],['TIME 2',['Gabriel','Du','rosivaldo'],3,true]]},
 };
